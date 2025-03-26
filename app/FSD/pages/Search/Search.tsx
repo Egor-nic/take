bb5a3d48904022754965a53344a6298283c08700
egor <egornick98@gmail.com>
2025-03-26 18:23:33 +0300

    fix: svg

--- app/FSD/pages/Search/Search.tsx
+++ app/FSD/pages/Search/Search.tsx
@@ -2,6 +2,12 @@ import Filters from "~/FSD/entities/Filters/Filters";
 import Products from "~/FSD/entities/Products/Products";
 import Container from "~/FSD/shared/ui/wrappers/Container/Container";
 
+export function meta() {
+  return [
+    { title: 'TAKE' },
+  ];
+}
+
 export default function Search() {
   return (
     <>
--- app/FSD/shared/ui/Input/Input.tsx
+++ app/FSD/shared/ui/Input/Input.tsx
@@ -5,10 +5,10 @@ export default function Input() {
     <form className={styles.form}>
         <input className={styles.input} type="text" placeholder="Начать поиск" />
         {/* <label className={styles.label} htmlFor="">label</label> */}
-        <button className={styles.btn} type="submit">
+        <button className={styles.btn} type="button">
           <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="12.2659" cy="11.7666" r="8.98856" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M18.5195 18.4851L22.0436 22" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <circle cx="12.2659" cy="11.7666" r="8.98856" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M18.5195 18.4851L22.0436 22" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
         </button>
     </form>
